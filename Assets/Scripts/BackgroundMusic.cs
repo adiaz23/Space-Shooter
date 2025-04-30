f8948dc007f28ea2bca59b5d45d5ca3e8f5ab4b1
Arianna Diaz <aldiaz0223@gmail.com>
2025-04-30 14:22:10 -0400

    Fix issue with audio listener

--- Assets/Scripts/BackgroundMusic.cs
+++ Assets/Scripts/BackgroundMusic.cs
@@ -1,15 +1,25 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class BackgroundMusic : MonoBehaviour
 {
     [SerializeField] private BackgroundMusic bgMusic;
+    [SerializeField] private AudioMixer audioMixer;
 
     void Awake(){
         if(FindObjectsByType<BackgroundMusic>(FindObjectsSortMode.None).Length > 1)
            Destroy(gameObject);
         DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        float savedMusicVolume = PlayerPrefs.GetFloat("musicVolume", 1f);
+        float savedSFXVolume = PlayerPrefs.GetFloat("sFXVolume", 1f);
+        audioMixer.SetFloat("MusicVolume", Mathf.Log10(savedMusicVolume) * 20);
+        audioMixer.SetFloat("SFXVolume", Mathf.Log10(savedSFXVolume) * 20);    
     }
 
     void Update()
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,24 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject gameOverUI;
+    [SerializeField] private GameObject pauseUI;
     [SerializeField] private Player player;
+    private static bool isPaused = false;
 
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(isPaused){
+                 Resume();
+            } else {
+                Pause();
+            }     
+        }
+    }
     public void GameOver(){
         gameOverUI.SetActive(true);
     }
@@ -20,10 +33,27 @@ public class GameManager : MonoBehaviour
 
     public void StartGame(){
          SceneManager.LoadScene("Game");
+         Resume();
     }
 
     public void GoToOptionsMenu(){
-        SceneManager.LoadScene("Options");
+        SceneManager.LoadScene("Options", LoadSceneMode.Additive);
+    }
+
+    public void Back(){
+        SceneManager.UnloadSceneAsync("Options");
+    }
+
+    public void Resume(){
+        pauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    void Pause(){
+        pauseUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
     }
 
     public void Quit(){
