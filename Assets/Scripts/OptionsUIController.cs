5e2ae8cde326e59729a8c14d83bbb9f66caaac19
Arianna Diaz <aldiaz0223@gmail.com>
2025-04-30 12:41:21 -0400

    Add SFX Volume controller

--- Assets/Scripts/OptionsUIController.cs
+++ Assets/Scripts/OptionsUIController.cs
@@ -1,13 +1,37 @@
+using System;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class OptionsUIController : MonoBehaviour
 {
   
     [SerializeField] AudioMixer audioMixer;
+    [SerializeField] Slider[] sliders;
 
-    public void SliderChange(float volume){
-        audioMixer.SetFloat("MasterVolume", volume);
+    private void Start(){
+        if(PlayerPrefs.HasKey("musicVolume")){
+            LoadVolume();  
+        } else{
+           SetMusicVolume();
+           SetSFXVolume();
+        }
     }
 
+    public void SetMusicVolume(){
+        float volume = sliders[0].value;
+        audioMixer.SetFloat("MusicVolume", Mathf.Log10(volume)*20);
+        PlayerPrefs.SetFloat("musicVolume", volume);
+    }
+
+    public void SetSFXVolume(){
+         float volume = sliders[1].value;
+        audioMixer.SetFloat("SFXVolume", Mathf.Log10(volume)*20);
+        PlayerPrefs.SetFloat("sFXVolume", volume);
+    }
+
+    private void LoadVolume(){
+        sliders[0].value = PlayerPrefs.GetFloat("musicVolume");
+        sliders[1].value = PlayerPrefs.GetFloat("sFXVolume");
+    }
 }
