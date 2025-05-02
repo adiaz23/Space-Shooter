1594eaa49e98cdb7577a53af31010aca312722fe
Arianna Diaz <aldiaz0223@gmail.com>
2025-05-02 14:00:03 -0400

    Add function to Adjust camera Width depending of the screen size

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -10,11 +10,19 @@ public class Parallax : MonoBehaviour
     void Start()
     {
         initialPosition = transform.position;
+        LockCameraWidthToImage();
     }
 
     void Update()
     {
        float substract =  speed * Time.time % imageWidth;
        transform.position = initialPosition + substract * direction;
+    }
+
+    private void LockCameraWidthToImage(){
+        Camera cam = Camera.main;
+        float aspectRatio = (float)Screen.width / Screen.height;
+        float requireHeight = imageWidth / aspectRatio;
+        cam.orthographicSize = requireHeight / 2f;
     }
 }
